203a152f761ac61aeae5d791122f702cc1e5f927
Lisanna <lisanna.paladin@gmail.com>
2025-07-27 18:19:00 +0200

    fix repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/yourrepo/', // ← replace with your repo name
+  base: '/TruckLoad/', // ← replace with your repo name
   plugins: [react()],
 });
